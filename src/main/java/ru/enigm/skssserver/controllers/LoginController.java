dc9ede56cf8b8b374c0ea585e2c2f21ecbb41107
Ural Nigmatullin <Nigmatullin.U.R@sberbank.ru>
2018-06-19 15:41:30 +0300

    added redirect to admin panel

--- src/main/java/ru/enigm/skssserver/controllers/LoginController.java
+++ src/main/java/ru/enigm/skssserver/controllers/LoginController.java
@@ -1,5 +1,9 @@
 package ru.enigm.skssserver.controllers;
 
+import ru.enigm.skssserver.data.UserRepositoryImpl;
+import ru.enigm.skssserver.domain.UserService;
+import ru.enigm.skssserver.domain.UserServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +12,24 @@ import java.io.IOException;
 
 public class LoginController extends HttpServlet {
 
+    private static final String USERNAME_PARAMETER = "username";
+    private static final String PASSWORD_PARAMETER = "password";
+
+    private UserService userService = new UserServiceImpl(new UserRepositoryImpl());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter(USERNAME_PARAMETER);
+        String password = req.getParameter(PASSWORD_PARAMETER);
+
+        if (userService.checkPasswordAndLogin(username, password)){
+            req.getRequestDispatcher("/admin.jsp").forward(req, resp);
+        }
+        super.doPost(req, resp);
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Login page</h2>
+<form action="#" method="post">
+    <p>Login: <input type="text" name="username"/></p>
+    <p>Password: <input type="password" name="password"/></p>
+    <input type="submit" value="log in"/>
+</form>
 </body>
 </html>
